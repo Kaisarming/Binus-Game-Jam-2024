ae8e4520108062eb086d394a688e2d6b0629ffb1
syarif <87380548+muhammadsrf@users.noreply.github.com>
2024-01-07 21:36:24 +0700

    Setup Jump on Spesific Level

--- Assets/Game Folders/Scripts/SetupState.cs
+++ Assets/Game Folders/Scripts/SetupState.cs
@@ -8,6 +8,18 @@ namespace Game_Folders.Scripts
         private void Start()
         {
             GameManager.Instance.ChangeState(Gamestate.Level);
+            var player = GameObject.FindWithTag("Player").GetComponent<Player>();
+
+            // set player jump 
+            if (GameManager.Instance.GetActiveLevelData().levelIndex == 2 ||
+                GameManager.Instance.GetActiveLevelData().levelIndex == 3)
+            {
+                player.jump = 24;
+            }
+            else
+            {
+                player.jump = 18;
+            }
         }
     }
 }
